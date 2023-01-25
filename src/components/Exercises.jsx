82764582c1b1eceb291123e4f692c9924dcb4578
Aditya <thisisaadityaa@gmail.com>
2023-01-25 23:09:37 +0530

    adding components

--- src/components/Exercises.jsx
+++ src/components/Exercises.jsx
@@ -3,9 +3,12 @@ import Pagination from '@mui/material/Pagination';
 import { Box, Stack, Typography} from '@mui/material/';
 
 import { exerciseOptions, fetchData } from '../utils/fetchData';
+import ExerciseCard from './ExerciseCard';
 
 
 const Exercises = ({ exercises, setExercises, bodyPart }) => {
+   
+
   return (
     <Box id="exercises"
       sx={{mt: { lg: '110px '}}}
@@ -15,6 +18,13 @@ const Exercises = ({ exercises, setExercises, bodyPart }) => {
       <Typography variant="h3" mb="46px">
         Showing Results
       </Typography>
+      <Stack direction="row" sx={{ gap: { lg: '110px', xs: '50px'}}} flexWrap="wrap" justifyContent="center">
+
+        {exercises.map((exercise, index) => (
+          <p>{exercise.name} </p>
+        ))}
+
+      </Stack>  
 
     </Box>
   )
