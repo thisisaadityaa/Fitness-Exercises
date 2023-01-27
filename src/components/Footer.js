97f2be15040d0a3a8c8830c35620ab2d600e9ee6
Aditya <thisisaadityaa@gmail.com>
2023-01-27 12:59:44 +0530

    finalising

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import { Box, Stack, Typography } from '@mui/material';
 import Logo from '../assets/images/Logo-1.png';
 
 const Footer = () => (
-  <Box mt="80px" bgcolor="#FFF3F4">
+  <Box mt="80px" bgColor="#FFF3F4">
     <Stack gap="40px" sx={{ alignItems: 'center' }} flexWrap="wrap" px="40px" pt="24px">
       <img src={Logo} alt="logo" style={{ width: '200px', height: '41px' }} />
     </Stack>
